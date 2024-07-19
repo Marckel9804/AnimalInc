0ca05b7a7b1cad72eaf32a4b58106b4575f51777
Marckel9804 <ckdgh10421@gmail.com>
2024-07-19 17:31:00 +0900

    first commit

---
+++ src/main/java/kr/bit/animalinc/AnimalIncApplication.java
@@ -0,0 +1,13 @@
+package kr.bit.animalinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalIncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalIncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/bit/animalinc/AnimalIncApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.bit.animalinc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalIncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
