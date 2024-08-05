5c7a8ea88eb863b1f7e87cec30184f65e987a75a
Marckel9804 <ckdgh10421@gmail.com>
2024-08-05 18:05:44 +0900

    before merge

---
+++ src/main/java/kr/bit/animalinc/dto/board/BoardWriteDTO.java
@@ -0,0 +1,13 @@
+package kr.bit.animalinc.dto.board;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardCommunityDTO {
+    private String title;
+    private String bcCode;
+}
--- src/main/java/kr/bit/animalinc/service/board/BoardService.java
+++ src/main/java/kr/bit/animalinc/service/board/BoardService.java
@@ -1,27 +1,2 @@
-package kr.bit.animalinc.service.board;
-
-import kr.bit.animalinc.entity.board.BoardFAQ;
-import kr.bit.animalinc.repository.board.BoardCommunityRepository;
-import kr.bit.animalinc.repository.board.BoardFAQRepository;
-import kr.bit.animalinc.repository.board.CommentRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
-@Service
-@Slf4j
-@RequiredArgsConstructor
-public class BoardService {
-
-    private final BoardCommunityRepository boardCommunityRepository;
-    private final BoardFAQRepository boardFAQRepository;
-    private final CommentRepository commentRepository;
-
-    @Transactional
-    public List<BoardFAQ> getBoardFAQs() {
-        return boardFAQRepository.findAll();
-    }
+package kr.bit.animalinc.service.board;public class BoardService {
 }
