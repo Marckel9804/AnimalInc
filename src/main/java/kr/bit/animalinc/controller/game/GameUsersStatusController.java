1f75d3e11396beaa86d6fe66d60a9c0c38fe0fc5
dhxorud <xorud0711@naver.com>
2024-08-13 10:18:41 +0900

    qwe

--- src/main/java/kr/bit/animalinc/controller/game/GameUsersStatusController.java
+++ src/main/java/kr/bit/animalinc/controller/game/GameUsersStatusController.java
@@ -3,6 +3,7 @@ package kr.bit.animalinc.controller.game;
 import kr.bit.animalinc.entity.game.GameRoom;
 import kr.bit.animalinc.repository.game.GameRoomRepository;
 import kr.bit.animalinc.service.game.GameUsersStatusService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import org.slf4j.LoggerFactory;
 @RestController
 @RequestMapping("/game")
 @CrossOrigin("*")
+@Slf4j
 public class GameUsersStatusController {
 
     // Logger 선언
@@ -26,9 +28,10 @@ public class GameUsersStatusController {
     @PostMapping("/insertUserStatus")
     public ResponseEntity<String> insertUserStatus(@RequestParam String gameRoomId, @RequestParam long userNum) {
         // 전달된 값을 로그에 출력
-        logger.info("Received request to insert user status with gameRoomId: {} and userNum: {}", gameRoomId, userNum);
+        log.info("Received request to insert user status with gameRoomId: {} and userNum: {}", gameRoomId, userNum);
 
-        gameUsersStatusService.saveUserStatus(gameRoomRepository.findById(gameRoomId).get(), userNum);
+        GameRoom gameRoom = gameRoomRepository.findById(gameRoomId).get();
+        gameUsersStatusService.saveUserStatus(gameRoom, userNum);
         return ResponseEntity.ok("User status inserted successfully");
     }
 }
