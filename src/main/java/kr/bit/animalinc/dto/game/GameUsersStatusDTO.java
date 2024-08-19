71973c3c16b257ad0b2e8bef549c1e0083977e52
JEONYUTAK <btt0408@gmail.com>
2024-08-19 18:39:20 +0900

    bp

--- src/main/java/kr/bit/animalinc/dto/game/GameUsersStatusDTO.java
+++ src/main/java/kr/bit/animalinc/dto/game/GameUsersStatusDTO.java
@@ -46,7 +46,7 @@ public class GameUsersStatusDTO {
     private int cash;
     private boolean isMe = false;
     private String nickName;
-    private String shortSellRecord;
+    private int newsCount;
 
     public static GameUsersStatusDTO toGameUserStatusDTO(GameUsersStatus gameUsersStatus) {
         return GameUsersStatusDTO.builder()
@@ -81,6 +81,6 @@ public class GameUsersStatusDTO {
                 .tech4(gameUsersStatus.getTech4())
                 .cash(gameUsersStatus.getCash())
                 .nickName("ananymous")
-                .isMe(false).shortSellRecord("").build();
+                .isMe(false).newsCount(gameUsersStatus.getNewsCount()).build();
     }
 }
